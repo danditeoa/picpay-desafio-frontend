48ced26edb1799011cb34cf15c3031bf4ce77ba4
DandiTeoa <taci.dandi@gmail.com>
2022-10-22 18:07:28 -0300

    feat(login): add login style component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,8 @@
 import { useAtom } from "jotai"
 import { isUserLoggedAtom } from "../../atoms/login.atom"
+import { LoginImageIcon } from "../Icons"
+import LogoPayFriends from "../LogoPayFriends/LogoPayFriends"
+import styles from './login.module.scss'
 
 const Login = () => {
   const [isUserLogged, setIsUserLogged] = useAtom(isUserLoggedAtom)
@@ -9,15 +12,15 @@ const Login = () => {
   //
 
   return (
-    <div
-      style={{
-        height: 300,
-        width: 300,
-        backgroundColor: "#666",
-      }}
-    >
-      <h1>meu login</h1>
-      <button onClick={() => setIsUserLogged(true)}> login </button>
+    <div className={styles.container}>
+      <div className="actionSection">
+        <LogoPayFriends />
+        <h1 className={styles.title}>Bem vindo de volta</h1>
+        <input className={styles.loginInput} type="email" /> <br />
+        <input className={styles.loginInput} type="password" /> <br />
+        <button className={styles.submit} onClick={() => setIsUserLogged(true)}> login </button>
+      </div>
+      <LoginImageIcon />
     </div>
   )
 }
