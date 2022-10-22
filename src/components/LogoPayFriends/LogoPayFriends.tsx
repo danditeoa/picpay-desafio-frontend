5e23f06b9548182b326a24dab1b66d0fd7bdcdf9
DandiTeoa <taci.dandi@gmail.com>
2022-10-22 17:31:33 -0300

    feat(logopayfriends): create style for component payfriends logo

---
+++ src/components/LogoPayFriends/LogoPayFriends.tsx
@@ -0,0 +1,12 @@
+import styles from '../Header/header.module.scss'
+
+const LogoPayFriends = () => {
+
+  return (
+    <div className={styles.payfriends}>
+        <b className={styles.pay}>Pay</b>Friends
+    </div>
+  );
+};
+
+export default LogoPayFriends;
