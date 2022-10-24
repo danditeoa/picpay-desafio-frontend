8f872b6bc85c9046e307c80d912524ce9458285b
DandiTeoa <taci.dandi@gmail.com>
2022-10-24 09:09:09 -0300

    feat(header): create logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,24 @@
+import { useAtom } from 'jotai';
+import { isUserLoggedAtom } from '../../atoms/login.atom';
 import styles from '../Header/header.module.scss'
+import LogoPayFriends from '../LogoPayFriends/LogoPayFriends';
 
 const Header = () => {
+  const [_, setIsUserLogged] = useAtom(isUserLoggedAtom)
 
   return (
-    <div className={styles.container}> 
-      <span className={styles.payfriends}><b className={styles.pay}>Pay</b>Friends</span>
-    </div>
+    <header className={styles.container}> 
+      <LogoPayFriends />
+      <a onClick={() => setIsUserLogged(false)} className={styles.logout}>
+        <img
+          className={styles.profilePicture}
+          src="https://image.shutterstock.com/image-photo/profile-picture-smiling-millennial-asian-600w-1836020740.jpg"
+          alt="imagem do perfil do usuario"
+        />
+        <p>DESLOGAR</p>
+	    </a>
+    </header>
   );
 };
 
-export default Header;
+export default Header
