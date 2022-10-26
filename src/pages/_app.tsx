cd9e6113540f3eb017c048ca870e78e1c08909d0
Dandi <33880852+danditeoa@users.noreply.github.com>
2022-10-26 10:40:10 -0300

    fix(app): descriptions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,18 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>picpay me contrata</title>
+        <title>PicPayFriends</title>
         <link rel="icon-pic-pay" href="/public/next/icons/favicon.ico" />
         <meta name="descrição" content="desafio técnico frontent do picpay #mecontrata" />
+
+        <title>PicPayFriends</title>
+        <link rel="picpayfriends-icon" href="/public/next/icons/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#fafafa" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components for picpay test"
+        />
       </Head>
       <Component {...pageProps} />
     </>
